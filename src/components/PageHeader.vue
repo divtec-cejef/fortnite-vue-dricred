6da3e45a675c004c486aab1e12e42660bb0112d3
Rédouane Drici <redouane.drici@divtec.ch>
2024-11-14 15:00:57 +0100

    ajout nouvelle branche

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,14 @@
+<template>
+<header>
+<nav>
+  <ul class="navigation">
+    <li class="actif"><a href="index.html">Accueil</a></li>
+    <li><a href="_sources/presentation.html">Présentation de Fortnite</a></li>
+    <li><a href="_sources/nouvelle-carte.html">Nouvelle Carte</a></li>
+  </ul>
+</nav>
+<h1>Fortnite - Armes (Battle Royale)</h1>
+</header>
+</template>
+<script setup>
+</script>
